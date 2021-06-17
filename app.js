7b04eb7c101c06b0cb2d0a52544b83eca56667ff
prerna <prernasmahabare24@gmail.com>
2021-06-17 20:46:19 +0530

    Initial commit

--- app.js
+++ app.js
@@ -5,15 +5,11 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const app = express();
 const bodyparser = require("body-parser");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use("/static", express.static('static'));
 app.use(express.urlencoded());
 
-<<<<<<< HEAD
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-=======
->>>>>>> 25695d837f240f7d7ee89b57d3d3238359fe26da
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
