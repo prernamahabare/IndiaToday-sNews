25695d837f240f7d7ee89b57d3d3238359fe26da
Prerna Mahabare <84230099+prernamahabare@users.noreply.github.com>
2021-06-17 20:04:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const port = 5000;
 app.use("/static", express.static('static'));
 app.use(express.urlencoded());
 
-app.use(favicon(path.join(__dirname, 'src', 'icon.ico')));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
