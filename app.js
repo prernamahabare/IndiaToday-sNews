cfee55067b2b30d81b22d67c71e8a94d36cd2f41
Prerna Mahabare <84230099+prernamahabare@users.noreply.github.com>
2021-06-18 11:15:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ const express = require("express");
 const app = express();
 const http = require('http');
 const path = require('path');
-
 const server = http.createServer(app);
 const port = process.env.PORT || 5000;
 
-const app = express();
-const bodyparser = require("body-parser");
-const port = process.env.PORT || 5500;
 
 app.use("/static", express.static('static'));
 app.use(express.urlencoded());
