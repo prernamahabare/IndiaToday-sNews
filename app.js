8371630640574aeaff60df3e6b7754d73d62d30f
Prerna Mahabare <84230099+prernamahabare@users.noreply.github.com>
2021-06-19 11:01:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,23 +9,19 @@ app.use(cors());
 app.use("/static", express.static('static'));
 app.use(express.urlencoded());
 
-<<<<<<< HEAD
- app.listen(80, function () {
+app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 80')
-  })
-  
-=======
+})
+
 app.get('/', function (req, res) {
     console.log(res.headersSent) // false
     console.log(res.headersSent) // true
-  })
+})
   
-  app.listen(80, function () {
+app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 80')
-  })
+})
 
- 
->>>>>>> 7ddc5d0ed29bea20c3476eb043e29beeb5c69850
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
