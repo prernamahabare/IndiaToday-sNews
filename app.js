06a3f85e6f2dc3375a7a60e22ec4fc1f7f42bfaa
prerna <prernasmahabare24@gmail.com>
2021-06-19 10:56:29 +0530

    initial commit

--- app.js
+++ app.js
@@ -9,28 +9,10 @@ app.use(cors());
 app.use("/static", express.static('static'));
 app.use(express.urlencoded());
 
-app.get('/', function (req, res) {
-<<<<<<< HEAD
-    console.log(res.headersSent) // false
-    res.send(xhr.send());
-    console.log(res.headersSent) // true
-  })
-
-app.get('/products/:id', function (req, res, next) {
-    res.json('This is CORS-enabled for all origins!');
-  })
-  
-  app.listen(80, function () {
+ app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 80')
   })
-=======
-  console.log(res.headersSent) // false
-  res.send(xhr.send());
-  console.log(res.headersSent) // true
-})
->>>>>>> 0fc45e3626687e355563ef0822d3b319a596e8cf
-
-
+  
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
